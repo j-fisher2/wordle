7c17204574d92ceec3e06a7d80dc36d64c8d23a1
Justin Fisher <justinfisher340@gmail.com>
2025-11-24 14:34:22 -0500

    add baseGameObject schema - internal use

--- server/validators/schemas.js
+++ server/validators/schemas.js
@@ -4,4 +4,11 @@ const guessBodySchema = z.object({
   guess: z.string().regex(/^[A-Z]+$/)
 });
 
-module.exports = {guessBodySchema}
+const baseGameObject = z.object({
+  gameWord: z.string().regex(/^[A-Z]+$/),
+  wordLength: z.number().int().positive(),
+  maxAttempts: z.number().int().positive(),
+  status: z.enum(["IN_PROGRESS", "WIN", "LOSS"])
+});
+
+module.exports = {guessBodySchema, baseGameObject}
