6eb76a2e9c7756a641a0fb58170beb68ed4d6bb2
Justin Fisher <justinfisher340@gmail.com>
2025-11-23 22:22:23 -0500

    add root server file to fascilitate api endpoints

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const apiRoutes = require('./routes/api');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
