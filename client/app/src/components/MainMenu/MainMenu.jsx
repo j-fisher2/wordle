62ebbd0816d1f438bcf414c0c46ada6ba0b8c868
Justin Fisher <justinfisher340@gmail.com>
2025-11-25 18:06:10 -0500

    update main menu component to include offline checkbox functionality

--- client/app/src/components/MainMenu/MainMenu.jsx
+++ client/app/src/components/MainMenu/MainMenu.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import "./MainMenu.css";
 
-const MainMenu = ({ onStartGame }) => (
-    <div className="menu">
-        <h1 className="title">Play Wordle</h1>
-        <img src="/Wordle_Logo.svg" width="40%"/>
-        <button className="button" onClick={onStartGame}>
-            Start Game
-        </button>
+const Checkbox = ({ label, checked, onChange }) => {
+  const handleChange = (e) => {
+    if (onChange) onChange();
+  };
+
+  return (
+    <div>
+        <label>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+        {label}
+        </label>
+    </div>
+  );
+};
+
+const MainMenu = ({ onStartGame, online, setOnline }) => (
+    <div>
+        <div>
+            <h1 className="title">Play Wordle</h1>
+            <img src="/Wordle_Logo.svg" width="40%"/>
+            <button className="button" onClick={onStartGame}>
+                Start Game
+            </button>
+        </div>
+        <div className="offline-select">
+            <Checkbox label={"Play Offline"} checked={!online} onChange={() => setOnline(online => !online)} />
+        </div>
     </div>
 );
 
