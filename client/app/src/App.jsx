b0b81c7310352998c067ddd0b5b105ffc45858da
Justin Fisher <justinfisher340@gmail.com>
2025-11-25 16:49:26 -0500

    cleanup unused util function within App

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -5,18 +5,6 @@ import { useGame } from './GameContext.jsx';
 import { GAME_CONFIG } from './config.js';
 import evaluateGuessSubmission from './utils/evaluateGuess.js';
 
-export const CONFIG = {
-  MAX_GUESSES: 6,
-  WORD_LENGTH: 5,
-  WORDS: ['APPLE', 'BEACH', 'CHAIR', 'DANCE', 'EAGLE', 'FLAME', 'GRAPE', 'HEART', 'IDEAL', 'JELLY', 'KNIFE', 'LEMON', 'MANGO', 'NIGHT', 'OCEAN', 'PLANT', 'QUEEN', 'RIVER', 'STONE', 'TIGER', 'URBAN', 'VIVID', 'WATCH', 'XENON', 'YOUTH', 'ZEBRA', 'ANGRY', 'BRICK', 'CABLE', 'DRIVE', 'EXTRA', 'FAITH', 'GIANT', 'HAPPY', 'INDEX', 'JOINT', 'KARMA', 'LIGHT', 'MARCH', 'NOVEL', 'OASIS', 'PEARL', 'QUICK', 'RALLY', 'SUGAR', 'TABLE', 'UNION', 'VOTER', 'WATER', 'YIELD', 'ACTOR', 'BAKER', 'CANDY', 'DREAM', 'EAGER', 'FANCY', 'GLOBE', 'HOUSE', 'IMAGE', 'JUICE', 'KARMA', 'LABEL', 'MUSIC', 'NOBLE', 'OTHER', 'PRIDE', 'QUIET', 'ROAST', 'SMILE', 'TREND', 'ALERT', 'BLESS', 'CLIMB', 'DRILL', 'EARTH', 'FROST', 'GRILL', 'HOVER', 'IDEAL', 'JEWEL', 'KNEEL', 'LAUGH', 'MODEL', 'NERVE', 'OFFER', 'PARTY', 'QUAKE', 'ROUGH', 'SHINE', 'TOUGH', 'AWAKE', 'BRAVE', 'CRANE', 'DOUGH', 'EVERY', 'FRAME', 'GREAT', 'HAPPY', 'INNER', 'JOINT']
-};
-
-
-const initializeAnswer = (wordList = GAME_CONFIG.WORDS) => {
-  const word = wordList[Math.floor(Math.random() * wordList.length)];
-  return word
-}
-
 const GameOver = ({message, resetGameStateCallback}) => {
   return (
     <div>
@@ -28,10 +16,6 @@ const GameOver = ({message, resetGameStateCallback}) => {
 
 function App() {
   const {gameState, setGameState, startGame, online} = useGame();
-
-  const resetGameState = () => {
-    setGameState(null);
-  }
 
   return (
     <div className='container'>
