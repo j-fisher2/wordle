d287b73ddc9c8d2bcb8b5eedd74421dd4255faf3
Justin Fisher <justinfisher340@gmail.com>
2025-11-25 18:06:46 -0500

    update App component to pass online state as top-level to main menu component

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -4,23 +4,16 @@ import Game from './components/Game/Game.jsx';
 import { useGame } from './GameContext.jsx';
 import { GAME_CONFIG } from './config.js';
 import evaluateGuessSubmission from './utils/evaluateGuess.js';
-
-const GameOver = ({message, resetGameStateCallback}) => {
-  return (
-    <div>
-      <h1>{message}</h1>
-      <button onClick={resetGameStateCallback}>Play Again</button>
-    </div>
-  )
-}
+import { useState } from "react";
 
 function App() {
-  const {gameState, setGameState, startGame, online} = useGame();
+  const {gameState, setGameState, startGame } = useGame();
+  const [online, setOnline] = useState(true);
 
   return (
     <div className='container'>
       {!gameState ?
-        <MainMenu onStartGame={() => startGame(online ? "online" : "offline")}/> : <Game gameState={gameState} setGameState={setGameState} evaluateGuessSubmission={evaluateGuessSubmission} isOnline={online}/>
+        <MainMenu onStartGame={() => startGame(online ? "online" : "offline")} online={online} setOnline={setOnline} /> : <Game gameState={gameState} setGameState={setGameState} evaluateGuessSubmission={evaluateGuessSubmission} isOnline={online}/>
       }
 
     </div>
