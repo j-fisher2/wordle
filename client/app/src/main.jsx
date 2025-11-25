12e2f8740723837016e89ce283b53899682d92f4
Justin Fisher <justinfisher340@gmail.com>
2025-11-25 08:57:38 -0500

    wrap main component in context provider

--- client/app/src/main.jsx
+++ client/app/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { GameProvider } from './GameContext';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+      <GameProvider>
+        <App />
+      </GameProvider>
+  </StrictMode>
+);
