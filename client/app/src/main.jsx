1e36c122908eb3dba50f6cf3f818e716b3c7176a
Justin Fisher <justinfisher340@gmail.com>
2025-11-25 09:16:02 -0500

    remove browser router import

--- client/app/src/main.jsx
+++ client/app/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { GameProvider } from './GameContext';
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
