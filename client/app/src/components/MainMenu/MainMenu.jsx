7fee77e70982d9c6f875841ba93abe0442e504a8
Justin Fisher <justinfisher340@gmail.com>
2025-11-26 14:39:01 -0500

    udpate main menu component to set difficulty level through imported component

--- client/app/src/components/MainMenu/MainMenu.jsx
+++ client/app/src/components/MainMenu/MainMenu.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import "./MainMenu.css";
+import RadioGroup from "../RadioGroup/RadioGroup";
+import Checkbox from "../Checkbox/Checkbox";
 
-const Checkbox = ({ label, checked, onChange }) => {
-  const handleChange = (e) => {
-    if (onChange) onChange();
-  };
 
-  return (
-    <div>
-        <label>
-        <input type="checkbox" checked={checked} onChange={handleChange} />
-        {label}
-        </label>
-    </div>
-  );
-};
-
-const MainMenu = ({ onStartGame, online, setOnline }) => (
-    <div className='container'>
-            <h1 className="title">Play Wordle</h1>
-            <img src="/Wordle_Logo.svg" width="70rem"/>
-            <button className="button" onClick={onStartGame}>
-                Start Game
-            </button>
-        <div className="offline-select">
-            <Checkbox label={"Play Offline"} checked={!online} onChange={() => setOnline(online => !online)} />
+function MainMenu({ onStartGame, online, setOnline, difficulty, setDifficulty }) {
+    const DIFFICULTY_LEVELS = [
+        {label: "Normal", value: "NORMAL"},
+        {label: "Hard", value: "HARD"}
+    ]
+    return (
+        <div className='container'>
+                <h1 className="title">Play Wordle</h1>
+                <img src="/Wordle_Logo.svg" width="70rem"/>
+                <button className="button" onClick={onStartGame}>
+                    Start Game
+                </button>
+            <div className="offline-select">
+                <Checkbox label={"Play Offline"} checked={!online} onChange={() => setOnline(online => !online)} />
+            </div>
+            <div className="difficulty-select">
+                <h3>Difficulty Level</h3>
+                <RadioGroup name={"difficulty-select"} options={DIFFICULTY_LEVELS} value={difficulty} onChange={setDifficulty}/>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default MainMenu;
