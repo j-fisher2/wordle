d2ce8097fc96f51a2bfbae4c011f1171ab633315
Justin Fisher <justinfisher340@gmail.com>
2025-11-26 09:03:26 -0500

    update main stylesheet to justify items center correctly

--- client/app/src/components/MainMenu/MainMenu.jsx
+++ client/app/src/components/MainMenu/MainMenu.jsx
@@ -17,14 +17,12 @@ const Checkbox = ({ label, checked, onChange }) => {
 };
 
 const MainMenu = ({ onStartGame, online, setOnline }) => (
-    <div>
-        <div>
+    <div className='container'>
             <h1 className="title">Play Wordle</h1>
-            <img src="/Wordle_Logo.svg" width="40%"/>
+            <img src="/Wordle_Logo.svg" width="60rem"/>
             <button className="button" onClick={onStartGame}>
                 Start Game
             </button>
-        </div>
         <div className="offline-select">
             <Checkbox label={"Play Offline"} checked={!online} onChange={() => setOnline(online => !online)} />
         </div>
