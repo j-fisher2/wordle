548af3c16811a6c21649242635c01943698f772e
Justin Fisher <justinfisher340@gmail.com>
2025-11-27 11:47:51 -0500

    udpate base game object schema to include players array

--- server/validators/schemas.js
+++ server/validators/schemas.js
@@ -2,6 +2,7 @@ const { z } = require("zod");
 
 const guessBodySchema = z.object({
   guess: z.string().regex(/^[A-Z]+$/),
+  playerId: z.uuid()
 });
 
 const baseGameObject = z.object({
@@ -9,6 +10,7 @@ const baseGameObject = z.object({
   wordLength: z.number().int().positive(),
   maxAttempts: z.number().int().positive(),
   status: z.enum(["IN_PROGRESS", "WIN", "LOSS"]),
+  players: z.array(z.uuid())
 });
 
 const createGameSchema = z.object({
