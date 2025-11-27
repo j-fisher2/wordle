15e84942f9e4bcea69684daf0f07e8f64366e6b8
Justin Fisher <justinfisher340@gmail.com>
2025-11-26 20:30:00 -0500

    add difficulty parameter to create game controller, add new guess evaluation call for hard difficulty to update candidate word list

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,7 +1,7 @@
 const gameStore = require('../store/games');
 const {WORDS, WORD_LENGTH, GUESSES_PER_GAME} = require('../config/config');
 const { v4: uuidv4, validate: isUuid } = require('uuid');
-const {evaluateGuess, evaluateGameStatus} = require('./utils');
+const {evaluateGuess, evaluateGameStatus, cheatEvaluate, } = require('./utils');
 
 const fetchGame = (gameId) => {
   // abstract function - update to use centralized data/key-value store
@@ -10,8 +10,9 @@ const fetchGame = (gameId) => {
 
 const createGame = (req, res) => {
   const gameId = uuidv4();
-  const gameWord = WORDS[Math.floor(Math.random() * WORDS.length)]
-  const game = {gameWord: gameWord, wordLength: WORD_LENGTH, maxAttempts: GUESSES_PER_GAME, status: "IN_PROGRESS", pastGuesses: [], difficulty: req.body.difficulty}
+  const gameWord = WORDS[Math.floor(Math.random() * WORDS.length)];
+  const difficulty = req.body.difficulty;
+  const game = {gameWord: gameWord, wordLength: WORD_LENGTH, maxAttempts: GUESSES_PER_GAME, status: "IN_PROGRESS", pastGuesses: [], difficulty: difficulty, candidateWords: difficulty === "HARD" ? WORDS : []}
   gameStore.set(gameId, game);
 
   return res.json({ gameId: gameId, wordLength: WORD_LENGTH, maxAttempts: GUESSES_PER_GAME, status: game.status, pastGuesses: game.pastGuesses, difficulty: game.difficulty });
@@ -46,8 +47,16 @@ const guessWord = (req, res) => {
     });
   }
 
-  const {gameWord} = game;
-  const guessEvaluation = evaluateGuess(guess, gameWord, WORD_LENGTH);
+  const {gameWord, difficulty, candidateWords} = game;
+  var guessEvaluation;
+  if(difficulty === "HARD"){
+    const {pattern, newCandidates} = cheatEvaluate(guess, candidateWords,game.wordLength);
+    game.candidateWords = newCandidates;
+    guessEvaluation = pattern;
+  }
+  else{
+    guessEvaluation = evaluateGuess(guess, gameWord, WORD_LENGTH, difficulty);
+  }
 
   game.pastGuesses = [...(game.pastGuesses || []),{guess: guess, result: guessEvaluation}]
   game.status = evaluateGameStatus(game);
