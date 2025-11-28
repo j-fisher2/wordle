32133a801ce60b207f0b4646f538df6da321e565
Justin Fisher <justinfisher340@gmail.com>
2025-11-27 20:53:58 -0500

    update main menu component to allow conditional rendering based on start mode - new game vs join game

--- client/app/src/components/MainMenu/MainMenu.jsx
+++ client/app/src/components/MainMenu/MainMenu.jsx
@@ -2,27 +2,45 @@ import React from "react";
 import "./MainMenu.css";
 import RadioGroup from "../RadioGroup/RadioGroup";
 import Checkbox from "../Checkbox/Checkbox";
-
+import JoinGame from "../JoinGame/JoinGame";
+import { useState } from "react";
 
 function MainMenu({ onStartGame, online, setOnline, difficulty, setDifficulty }) {
     const DIFFICULTY_LEVELS = [
         {label: "Normal", value: "NORMAL"},
         {label: "Hard", value: "HARD"}
     ]
+    const [startMode, setStartMode] = useState(null);
+
     return (
         <div className='container'>
                 <h1 className="title">Play Wordle</h1>
                 <img src="/Wordle_Logo.svg" width="70rem"/>
-                <button className="button" onClick={onStartGame}>
-                    Start Game
-                </button>
-            <div className="offline-select">
-                <Checkbox label={"Play Offline"} checked={!online} onChange={() => setOnline(online => !online)} />
-            </div>
-            <div className="difficulty-select">
-                <h3>Difficulty Level</h3>
-                <RadioGroup name={"difficulty-select"} options={DIFFICULTY_LEVELS} value={difficulty} onChange={setDifficulty}/>
-            </div>
+                { (!startMode) &&
+                    <button className="button" onClick={() => setStartMode("NEW")}>
+                        Start a New Game
+                    </button>
+                }
+                { (!startMode) &&
+                    <button className="lower-button" onClick={() => setStartMode("JOIN")}>
+                        Join a Friend's Game
+                    </button>
+                }
+                {
+                startMode === "NEW" &&
+                <>
+                    <div className="offline-select">
+                        <Checkbox label={"Play Offline"} checked={!online} onChange={() => setOnline(online => !online)} />
+                    </div>
+                    <div className="difficulty-select">
+                        <h3>Difficulty Level</h3>
+                        <RadioGroup name={"difficulty-select"} options={DIFFICULTY_LEVELS} value={difficulty} onChange={setDifficulty}/>
+                    </div>
+                    <button className='button' onClick={onStartGame}>Start</button>
+                </>
+                }
+                {startMode === "JOIN" && <JoinGame />}
+                {startMode && <button className="lower-button" onClick={()=>setStartMode(null)}>Back</button>}
         </div>
     )
 };
