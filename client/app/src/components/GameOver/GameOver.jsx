78b45fefe656ff5fc2ca685cd9b3d5d0df9e55f1
Justin Fisher <justinfisher340@gmail.com>
2025-11-27 20:53:08 -0500

    update GameOver component to accept game state for up-to-date data and render score on UI for current player vs opponent

--- client/app/src/components/GameOver/GameOver.jsx
+++ client/app/src/components/GameOver/GameOver.jsx
@@ -1,8 +1,15 @@
-const GameOver = ({message, setGameState}) => {
+const GameOver = ({message, setGameState, gameState}) => {
   return (
     <div>
       <h1>{message}</h1>
-      <button onClick={() => setGameState(null)}>Play Again</button>
+        {Object.entries(gameState.scores).map(([playerId, score]) =>
+            playerId === localStorage.getItem("playerId") ? (
+                <div key={playerId}>Your score: {score}</div>
+            ) : (
+                <div key={playerId}>Opponent score: {score}</div>
+            )
+        )}
+      <button style={{ marginTop: "1rem" }} onClick={() => setGameState(null)}>Play Again</button>
     </div>
   )
 }
