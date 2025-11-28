90f356a9e30a4da4205c15c64f9c84c65f56feac
Justin Fisher <justinfisher340@gmail.com>
2025-11-27 20:52:29 -0500

    update Game component to poll game status from server

--- client/app/src/components/Game/Game.jsx
+++ client/app/src/components/Game/Game.jsx
@@ -1,14 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GameOver from "../GameOver/GameOver";
 
 export default function Game({ evaluateGuessSubmission, gameState ,setGameState, isOnline}){ // these should be props as they will differ from client side game vs client server game
     const [currentGuess, setCurrentGuess] = useState("");
+
+  useEffect(() => {
+    const fetchGameState = async () => {
+        if(gameState.status !== "IN_PROGRESS"){
+            return;
+        }
+      try {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/game/${gameState.gameId}`);
+        const data = await res.json();
+        console.log(data);
+        if (res.ok) {
+          setGameState({...gameState, ...data});
+        } else {
+          console.error("Server error:", data?.message);
+        }
+      } catch (err) {
+        console.error("Network error:", err);
+      }
+    };
+    
+    fetchGameState();
+
+    const interval = setInterval(fetchGameState, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
     
     return (
         <div className="container">
         {(gameState?.status === "IN_PROGRESS" || !gameState?.status ) &&
             <div className="in-progress">
             <h1>Wordle</h1>
+            <h5>Your Game ID: </h5>
+            <h5>{gameState.gameId}</h5>
 
             <div className="grid">
                 {Array.from({ length: gameState?.maxAttempts }).map((_, row) => {
@@ -45,7 +74,7 @@ export default function Game({ evaluateGuessSubmission, gameState ,setGameState,
             {gameState?.message && <p className="message">{gameState?.message}</p>}
             </div>
         }
-        {(gameState.status === "LOSS" || gameState.status === "WIN") && <GameOver message={gameState.message} setGameState={setGameState}/>}
+        {(gameState.status === "LOSS" || gameState.status === "WIN") && <GameOver message={gameState.status === "WIN"? "You win! 🎉" : "Out of guesses!"} setGameState={setGameState} gameState={gameState}/>}
         </div>
     );
 }
