bb9c5448dfe40d024ffa663ae91a2c7a9be894c3
Justin Fisher <justinfisher340@gmail.com>
2025-11-27 20:51:51 -0500

    add toast capability on failed start game network request

--- client/app/src/GameContext.jsx
+++ client/app/src/GameContext.jsx
@@ -23,14 +23,20 @@ export const GameProvider = ({ children }) => {
         if (mode === "online") {
             try {
                 const res = await fetch(GAME_API_ENDPOINT, {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({difficulty: difficultyLevel})});
+                  if (!res.ok) {
+                    const errorData = await res.json().catch(() => null);
+                    toast.error(errorData?.message || "Unknown error");
+                    return;
+                  }
                 const data = await res.json();
+                localStorage.setItem("playerId", data.playerId)
                 setGameState(data);
             } catch (err) {
                 console.error("Failed to fetch game:", err);
             }
         } else {
             if(difficultyLevel === "HARD"){
-              toast.error("Hard is available in online mode only!");
+              toast.error("Oops.. hard mode is available online only");
               return;
             }
             const offlineState = { ...defaultOfflineState, gameWord: GAME_CONFIG.WORDS[Math.floor(Math.random() * GAME_CONFIG.WORDS.length)]};
